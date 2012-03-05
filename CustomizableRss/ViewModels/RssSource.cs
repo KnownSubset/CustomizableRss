20526b5f9609fc25cb74b87b42b3df2c4fc6cfea
Nathan <nathan.dauber@gmail.com>
2012-03-05 08:59:18 -0600

    first commit

---
+++ CustomizableRss/ViewModels/RssSource.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.Runtime.Serialization;
+
+namespace CustomizableRss {
+    public class RssSource 
+    {
+        
+    }
+}
