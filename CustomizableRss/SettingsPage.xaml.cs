1ac9d6509438b64ce7eac7d7acf31832d08f28cb
Nathan Dauber <Nathan.Dauber@gmail.com>
2012-03-16 10:10:07 -0500

    have the rss feed page of the settings page prototyped, need to work on add/edit page

---
+++ CustomizableRss/SettingsPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CustomizableRss.ViewModels;
+using Microsoft.Phone.Controls;
+
+namespace CustomizableRss
+{
+    public partial class SettingsPage : PhoneApplicationPage
+    {
+        private readonly SettingsViewModel settingsViewModel = new SettingsViewModel();
+
+        public SettingsPage()
+        {
+            DataContext = settingsViewModel;
+            InitializeComponent();
+        }
+
+        private void EditRss(object sender, RoutedEventArgs e)
+        {
+            var rssItem = (sender as StackPanel).DataContext as RssFeed;
+            var isolatedStorageSettings = IsolatedStorageSettings.ApplicationSettings;
+            isolatedStorageSettings["rssFeed"] = rssItem;
+            settingsViewModel.RssFeeds.Remove(rssItem);
+            NavigationService.Navigate(new Uri("AddEditPage.xaml", UriKind.Relative));
+            NavigationService.Navigated += NavigatedBackToPage;
+        }
+
+        private void NavigatedBackToPage(object sender, NavigationEventArgs navigationEventArgs)
+        {
+            var isolatedStorageSettings = IsolatedStorageSettings.ApplicationSettings;
+            if (isolatedStorageSettings.Contains("rssFeed"))
+            {
+                var rssFeed = isolatedStorageSettings["rssFeed"] as RssFeed;
+                settingsViewModel.RssFeeds.Add(rssFeed);
+            }
+        }
+
+        private void DeleteRss(object sender, RoutedEventArgs e)
+        {
+            var rssItem = (sender as StackPanel).DataContext as RssFeed;
+            settingsViewModel.RssFeeds.Remove(rssItem);
+        }
+
+        private void AddRss(object sender, RoutedEventArgs e)
+        {
+            var isolatedStorageSettings = IsolatedStorageSettings.ApplicationSettings;
+            isolatedStorageSettings.Remove("rssFeed");
+            NavigationService.Navigate(new Uri("AddEditPage.xaml", UriKind.Relative));
+            NavigationService.Navigated += NavigatedBackToPage;
+        }
+    }
+}
---
+++ CustomizableRss/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO.IsolatedStorage;
+using System.Runtime.Serialization;
+
+namespace CustomizableRss.ViewModels
+{
+    public class SettingsViewModel : INotifyPropertyChanged
+    {
+        private const string RssFeedsKey = "rssFeeds";
+        private ObservableCollection<RssFeed> _rssFeeds;
+
+        public SettingsViewModel()
+        {
+            Items = new ObservableCollection<ItemViewModel>();
+            RssFeeds = new ObservableCollection<RssFeed>();
+        }
+
+
+        /// <summary>
+        /// A collection for ItemViewModel objects.
+        /// </summary>
+        public ObservableCollection<ItemViewModel> Items { get; private set; }
+
+        public ObservableCollection<RssFeed> RssFeeds
+        {
+            get { return _rssFeeds; }
+            private set
+            {
+                _rssFeeds = value;
+                NotifyPropertyChanged("RssFeeds");
+            }
+        }
+
+        public RssFeed RssFeed
+        {
+            get { return rssFeed; }
+            private set
+            {
+                rssFeed = value;
+                NotifyPropertyChanged("RssFeed");
+            }
+        }
+
+        public bool IsDataLoaded { get; private set; }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        /// <summary>
+        /// Creates and adds a few ItemViewModel objects into the Items collection.
+        /// </summary>
+        public void LoadData()
+        {
+            foreach (RssFeed loadRssFeed in LoadRssFeeds())
+            {
+                RssFeeds.Add(loadRssFeed);
+            }
+            IsDataLoaded = true;
+        }
+
+        private IEnumerable<RssFeed> LoadRssFeeds()
+        {
+            IsolatedStorageSettings isolatedStorageSettings = IsolatedStorageSettings.ApplicationSettings;
+            if (!isolatedStorageSettings.Contains(RssFeedsKey))
+            {
+                isolatedStorageSettings[RssFeedsKey] = new List<RssFeed>();
+            }
+            return isolatedStorageSettings[RssFeedsKey] as IList<RssFeed>;
+        }
+
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    #region Nested type: RequestState
+
+    [DataContract]
+    public class RssFeed
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public Uri Address { get; set; }
+    }
+
+    #endregion
+}
